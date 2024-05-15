dcccd3e4c05e34044264ae42e4deba486e6ed49b
Bin Xiao <bxiao103@uottawa.ca>
2024-05-15 08:52:22 -0700

    add teds.py

---
+++ teds.py
@@ -0,0 +1,253 @@
+# Copyright 2020 IBM
+# Author: peter.zhong@au1.ibm.com
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the Apache 2.0 License.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Apache 2.0 License for more details.
+
+import glob
+import distance
+from apted import APTED, Config
+from apted.helpers import Tree
+from lxml import etree, html
+from collections import deque
+from parallel import parallel_process
+from tqdm import tqdm
+import os
+
+noise_list = glob.glob("noise_list/*.jpg")
+
+class TableTree(Tree):
+    def __init__(self, tag, colspan=None, rowspan=None, content=None, *children):
+        self.tag = tag
+        self.colspan = colspan
+        self.rowspan = rowspan
+        self.content = content
+        self.children = list(children)
+
+    def bracket(self):
+        """Show tree using brackets notation"""
+        if self.tag == 'td':
+            result = '"tag": %s, "colspan": %d, "rowspan": %d, "text": %s' % \
+                     (self.tag, self.colspan, self.rowspan, self.content)
+        else:
+            result = '"tag": %s' % self.tag
+        for child in self.children:
+            result += child.bracket()
+        return "{{{}}}".format(result)
+
+
+class CustomConfig(Config):
+    @staticmethod
+    def maximum(*sequences):
+        """Get maximum possible value
+        """
+        return max(map(len, sequences))
+
+    def normalized_distance(self, *sequences):
+        """Get distance from 0 to 1
+        """
+        return float(distance.levenshtein(*sequences)) / self.maximum(*sequences)
+
+    def rename(self, node1, node2):
+        """Compares attributes of trees"""
+        if (node1.tag != node2.tag) or (node1.colspan != node2.colspan) or (node1.rowspan != node2.rowspan):
+            return 1.
+        if node1.tag == 'td':
+            if node1.content or node2.content:
+                return self.normalized_distance(node1.content, node2.content)
+        return 0.
+
+
+class TEDS(object):
+    ''' Tree Edit Distance basead Similarity
+    '''
+    def __init__(self, structure_only=False, n_jobs=1, ignore_nodes=None):
+        assert isinstance(n_jobs, int) and (n_jobs >= 1), 'n_jobs must be an integer greather than 1'
+        self.structure_only = structure_only
+        self.n_jobs = n_jobs
+        self.ignore_nodes = ignore_nodes
+        self.__tokens__ = []
+
+    def tokenize(self, node):
+        ''' Tokenizes table cells
+        '''
+        self.__tokens__.append('<%s>' % node.tag)
+        if node.text is not None:
+            self.__tokens__ += list(node.text)
+        for n in node.getchildren():
+            self.tokenize(n)
+        if node.tag != 'unk':
+            self.__tokens__.append('</%s>' % node.tag)
+        if node.tag != 'td' and node.tail is not None:
+            self.__tokens__ += list(node.tail)
+
+    def load_html_tree(self, node, parent=None):
+        ''' Converts HTML tree to the format required by apted
+        '''
+        global __tokens__
+        if node.tag == 'td':
+            if self.structure_only:
+                cell = []
+            else:
+                self.__tokens__ = []
+                self.tokenize(node)
+                cell = self.__tokens__[1:-1].copy()
+            new_node = TableTree(node.tag,
+                                 int(node.attrib.get('colspan', '1')),
+                                 int(node.attrib.get('rowspan', '1')),
+                                 cell, *deque())
+        else:
+            new_node = TableTree(node.tag, None, None, None, *deque())
+        if parent is not None:
+            parent.children.append(new_node)
+        if node.tag != 'td':
+            for n in node.getchildren():
+                self.load_html_tree(n, new_node)
+        if parent is None:
+            return new_node
+
+    def evaluate(self, pred, true):
+        ''' Computes TEDS score between the prediction and the ground truth of a
+            given sample
+        '''
+        if (not pred) or (not true):
+            return 0.0
+
+        parser = html.HTMLParser(remove_comments=True, encoding='utf-8')
+        pred = html.fromstring(pred, parser=parser)
+        true = html.fromstring(true, parser=parser)
+
+        #if pred.xpath('body/table') and true.xpath('body/table'):
+        if pred.xpath('//table') and true.xpath('//table'):
+            pred = pred.xpath('//table')[0]
+            true = true.xpath('//table')[0]
+            if self.ignore_nodes:
+                etree.strip_tags(pred, *self.ignore_nodes)
+                etree.strip_tags(true, *self.ignore_nodes)
+            n_nodes_pred = len(pred.xpath(".//*"))
+            n_nodes_true = len(true.xpath(".//*"))
+            n_nodes = max(n_nodes_pred, n_nodes_true)
+            tree_pred = self.load_html_tree(pred)
+            tree_true = self.load_html_tree(true)
+            distance = APTED(tree_pred, tree_true, CustomConfig()).compute_edit_distance()
+            return 1.0 - (float(distance) / n_nodes)
+        else:
+            return 0.0
+
+    def batch_evaluate(self, pred_json, true_json):
+        ''' Computes TEDS score between the prediction and the ground truth of
+            a batch of samples
+            @params pred_json: {'FILENAME': 'HTML CODE', ...}
+            @params true_json: {'FILENAME': {'html': 'HTML CODE'}, ...}
+            @output: {'FILENAME': 'TEDS SCORE', ...}
+        '''
+        #samples = true_json.keys()
+        samples = pred_json.keys()
+        if self.n_jobs == 1:
+            #scores = [self.evaluate(pred_json.get(filename, ''), true_json[filename]['html']) for filename in tqdm(samples)]
+            scores = []
+
+
+            for filename in tqdm(samples):
+            #for filename in samples:
+                tmp_name = filename.replace(".html", "_fig_cells.jpg")
+
+                if "noise_list/" + tmp_name in noise_list:
+                    score = 1
+                    scores.append(score)
+                    continue
+
+                score = self.evaluate(pred_json.get(filename, ''), true_json[filename]['html'])
+                #if score != 1:
+                #    print(score)
+                #    print(filename)
+                #    exit()
+                scores.append(score)
+        else:
+            #inputs = [{'pred': pred_json.get(filename, ''), 'true': true_json[filename]['html']} for filename in samples]
+            inputs = []
+            for filename in samples:
+                if filename in true_json.keys():
+                    inputs.append({'pred': pred_json.get(filename, ''), 'true': true_json[filename]['html']})
+
+
+
+            scores = parallel_process(inputs, self.evaluate, use_kwargs=True, n_jobs=self.n_jobs, front_num=1)
+        scores = dict(zip(samples, scores))
+        return scores
+
+def collect_html(html_dir, is_gt = False):
+    html_list = glob.glob(os.path.join(html_dir, "*.html"))
+
+    result_dict = {}
+    for file_path in html_list:
+        file_name = file_path.split("/")[-1]
+        with open(file_path) as f:
+            html_code = f.read()
+            if is_gt:
+                result_dict[file_name] = {"html": html_code}
+            else:
+                result_dict[file_name] = html_code
+
+    return result_dict
+
+if __name__ == '__main__':
+    import json
+    import pprint
+
+
+    #gt_dir = "/data/datasets/TSR_Detection/PubTables1M/PubTables1M_html_gt_structure_only_6classes"
+    #pred_dir = "/data/logs/pubtab1m_results_cascadercnn_pred_no_text"
+    #pred_dir = "/data/logs/pubtab1m_results_cascadercnn_pred_no_text_baseline"
+    #pred_dir = "/data/logs/pubtab1m_comp_results"
+    #pred_dir = "/data/logs/pub1m_results"
+
+    #gt_dir = "/data/logs/fintabnet_html_gt"
+    #pred_dir = "/data/logs/fintabnet_comp_results"
+
+    #gt_dir = "/data/logs/fintabnet_html_gt_with_text"
+    #gt_dir = "/data/logs/fintabnet_html_gt_with_text_6classes"
+    #gt_dir = "/data/logs/fintabnet_html_gt_no_text_6classes"
+
+    #gt_dir = "/data/logs/scitsr_html_gt_with_text_6classes"
+    #gt_dir = "/data/logs/scitsr_html_gt_no_text_6classes"
+
+    #pred_dir = "/data/logs/scitsr_results_6classes_comp_no_text"
+    #pred_dir = "/data/logs/scitsr_results_grid_no_text_6classes"
+
+    #pred_dir = "/data/logs/deformable_detr_results_scitsr_6classes_with_text"
+
+    #pred_dir = "/data/logs/pub1m_results"
+
+    #pred_dir = "/data/logs/fintabnet_6classes_sim_with_text"
+    #pred_dir = "/data/logs/fintanet_results_cascadercnn_pred_with_text_6classes"
+
+
+    #gt_dir = "/data/datasets/PubTabNet/val_gt_structure_only"
+    #pred_dir = "/data/datasets/PubTabNet/val_html_pred"
+    #pred_dir = "/data/datasets/PubTabNet/sim_val_pred"
+
+
+    #gt_dir = "/data/datasets/fintabnet_donut_test/gt_html"
+    #pred_dir = "/data/logs/donut_html_output"
+
+
+    pred_dict = collect_html(pred_dir, is_gt=False)
+    gt_dict = collect_html(gt_dir, is_gt = True)
+
+    #with open('sample_pred_selected.json') as fp:
+    #    pred_json = json.load(fp)
+    #with open('sample_gt_selected.json') as fp:
+    #    gt_json = json.load(fp)
+
+    teds = TEDS(n_jobs=80)
+    scores = teds.batch_evaluate(pred_dict, gt_dict)
+    #scores = teds.batch_evaluate(pred_json, gt_json)
+    pp = pprint.PrettyPrinter()
+    #pp.pprint(scores)
+    print("length of scores:", len(scores) , "final average score:", sum(scores.values()) / len(scores.values()))
